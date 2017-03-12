e9f27fe2d91a9e25fc163731688cf44d63f63837
Alexander <Alexander@ALEXANDER>
2017-03-12 15:47:08 -0500

    graficos agregados y funcionando

--- WindowsSubirDatosMysql/Form1.Designer.cs
+++ WindowsSubirDatosMysql/Form1.Designer.cs
@@ -28,12 +28,211 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.HorizontalLineAnnotation horizontalLineAnnotation1 = new System.Windows.Forms.DataVisualization.Charting.HorizontalLineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bntInsert = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textsql = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::WindowsSubirDatosMysql.Properties.Resources.loading;
+            this.pictureBox1.Location = new System.Drawing.Point(117, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 12);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // bntInsert
+            // 
+            this.bntInsert.Location = new System.Drawing.Point(0, 46);
+            this.bntInsert.Name = "bntInsert";
+            this.bntInsert.Size = new System.Drawing.Size(75, 23);
+            this.bntInsert.TabIndex = 10;
+            this.bntInsert.Text = "Ingresar";
+            this.bntInsert.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(6, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(46, 24);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(29, 13);
+            this.lblPath.TabIndex = 7;
+            this.lblPath.Text = "Path";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textsql);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 198);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Filtrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textsql
+            // 
+            this.textsql.AutoCompleteCustomSource.AddRange(new string[] {
+            "select",
+            "insert ",
+            "delete ",
+            "from ",
+            "and",
+            "where",
+            "having",
+            "group",
+            "table ",
+            "procedure",
+            "insert",
+            "create",
+            "alter",
+            "values",
+            "( )",
+            "now()",
+            "date_format()",
+            "str_to_date()",
+            "drop",
+            "if",
+            "exists ",
+            "view",
+            "auto_increment",
+            "int",
+            "char",
+            "varchar",
+            "date",
+            "datetime",
+            "tiemstamp",
+            "default"});
+            this.textsql.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.textsql.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textsql.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textsql.ForeColor = System.Drawing.SystemColors.Info;
+            this.textsql.Location = new System.Drawing.Point(3, 19);
+            this.textsql.Multiline = true;
+            this.textsql.Name = "textsql";
+            this.textsql.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textsql.Size = new System.Drawing.Size(252, 144);
+            this.textsql.TabIndex = 10;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.bntInsert);
+            this.groupBox2.Controls.Add(this.lblPath);
+            this.groupBox2.Location = new System.Drawing.Point(12, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(255, 110);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // chart1
+            // 
+            horizontalLineAnnotation1.Name = "HorizontalLineAnnotation1";
+            this.chart1.Annotations.Add(horizontalLineAnnotation1);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Right;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(313, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series1.ChartArea = "ChartArea1";
+            series1.EmptyPointStyle.Color = System.Drawing.Color.Red;
+            series1.EmptyPointStyle.ToolTip = "Resultados de la consulta";
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 2;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(719, 360);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 360);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button bntInsert;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textsql;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- WindowsSubirDatosMysql/Form1.cs
+++ WindowsSubirDatosMysql/Form1.cs
@@ -7,14 +7,268 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace WindowsSubirDatosMysql
 {
     public partial class Form1 : Form
-    {
+    { //variables importantes 
+        private string path;
+        private string myConnectionString = "server=127.0.0.1;uid=root;" +
+                "pwd=uta007008%;database=fisiedns;";
         public Form1()
         {
             InitializeComponent();
+            bntInsert.Enabled = false;
+            pictureBox1.Visible = false;
+           label2.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openDialog();
+        }
+        private void openDialog()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            //propiedades del openDialogo prar subir  archivos filtrados
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Files (*.csv)|*.csv";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    string dir = openFileDialog1.FileName;
+
+                    
+
+                    //Console.WriteLine("Original string: \"{0}\"", dir);
+                    //del path tomado remplazamos el \ por / para que sea legible desde mysql
+                    path = dir.Replace("\\","//");
+                    lblPath.Text = path;
+                    if (!lblPath.Equals(""))
+                    {
+                        bntInsert.Enabled = true;
+                    }
+                    //usando el insert para la importacion de los datos 
+                    
+                   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error,no se puede leer el archivo" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        public int insert(string path)
+        {
+            int resultado=0;
+
+                   
+         
+            try
+            {
+               MySqlConnection conn = new MySqlConnection(myConnectionString);
+                conn.Open();
+               string sql1= "LOAD DATA LOCAL INFILE '" + path + "' INTO TABLE dns.t1 " +
+  " FIELDS TERMINATED BY ';' " +
+    " LINES TERMINATED BY '\r\n' " +
+        " (fecha,cliente,puerto,dominios,addr) ";
+                //MessageBox.Show(sql1);
+                MySqlCommand cmd = new MySqlCommand(sql1,conn);
+                //cmd.CommandTimeout = 3000;
+
+                //comando sql que permite importar un archivo cvs a mysql
+
+                //en el caso de Fields termined by puede ser por (, o ;) depede de la version del excel
+                // entre parentesis van el nombre de las columnas que se van a introducir en la tabla
+
+
+
+
+               resultado= cmd.ExecuteNonQuery();
+                
+                conn.Close();
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error: " + ex.Message + " filas agregadas: " + resultado, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            
+            return resultado;
+
+
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntInsert_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Procesando...";
+            button1.Enabled = false;
+            pictureBox1.Visible = true;
+            bntInsert.Enabled = false;
+            int result =insert(path);
+            if (result >0)
+            {
+                label2.Text = "Proceso finalizado !";
+                pictureBox1.Visible = false;
+                button1.Enabled = true;
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+                label2.Text = "Intente nuevamente !";
+                button1.Enabled = true;
+            }
+
+           
+               
+                
+          
+
+            
+          
+        }
+        private DataTable exeSql(string sql)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                MySqlConnection cnn = new MySqlConnection(myConnectionString);
+                cnn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, cnn);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                adapter.Fill(dt);
+              
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error: "+ex.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            return dt;
+
+        }
+
+        public void llenarChart()
+        {
+            List<string> series1 = new List<string>();
+            series1.Add("men");
+            series1.Add("women");
+            series1.Add("boys");
+            series1.Add("girlds");
+
+            List<int> puntos = new List<int>();
+            puntos.Add(10);
+            puntos.Add(20);
+            puntos.Add(30);
+            puntos.Add(20);
+
+            string[] seriesArray = { "Cats", "Dogs" };
+            int[] pointsArray = { 1, 2 };
+
+            // Set palette.
+            chart1.Palette = ChartColorPalette.SeaGreen;
+
+            // Set title.
+            chart1.Titles.Add("Humans");
+
+            // Add series.
+            for (int i = 0; i < series1.Count; i++)
+            {
+                // Add series.
+                Series series = chart1.Series.Add(series1[i]);
+
+                // Add point.
+                series.Points.Add(puntos[i]);
+            }
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        
+            chart1.Palette = ChartColorPalette.SeaGreen;
+            DataTable table = new DataTable();
+            // Set title.
+            chart1.Titles.Add("Results");
+            table=exeSql(textsql.Text.Trim());
+
+            chart1.DataSource = table;
+
+            chart1.Series[0].XValueMember = "serie";
+            chart1.Series[0].YValueMembers = "puntos";
+            chart1.DataBind();
+
+            /**
+             * codigo para graficar extrayendo datos del datatable
+             * */
+            //foreach (DataRow row in table.Rows)
+            //     {
+
+            //     Series series = chart1.Series.Add(row["serie"].ToString());
+            //     series.Points.Add(int.Parse(row["puntos"].ToString()));
+            // }
+
+
+            //row[column] = row[column].ToString().Replace(@"\", @" ").Trim();
+
+
+        }
+
+        private void PrintColumnNames(DataTable table)
+        {
+
+
+            // cargar el nombre de las columnas
+            //foreach (DataColumn dc in table.Columns)
+            //{
+            //    String field1 = dc.ColumnName;
+            //    MessageBox.Show(field1);
+            //}
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (DataColumn column in table.Columns)
+                {
+                    //                     row[column] obtiene el valor de las columnas de la fila
+                    row[column] = row[0].ToString();
+                   // series.Points.Add(puntos[i]);
+                    row[column] = row[1].ToString().Replace(@"\", @" ").Trim();
+                }
+            }
         }
     }
 }
